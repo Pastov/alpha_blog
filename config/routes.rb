5dc72fb9733c83b17c61bcb9d32e21cb0d50fd0d
pastov <diego.acosta.m88@gmail.com>
2018-12-25 15:41:15 -0300

    adding listing articles, show, edit, defining index page and finally back links to index page

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   root 'pages#home'
   get  'about', to: 'pages#about'
 
+  resources :articles
+  #da la opcion de agregar, eliminar, editar, etc elementos en los articulos
+
+
 end
